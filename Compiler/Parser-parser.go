1f4e6bf7d168095c720e9a5867ce1fbdc36a1b64
Joviro Rocha <joviro_neto@hotmail.com>
2023-07-02 21:54:36 -0300

    Changes

--- Compiler/Parser-parser.go
+++ Compiler/Parser-parser.go
@@ -26,10 +26,11 @@ func Parser(filePtr *os.File, line int, column int) {
 		action, exists := SLR[state][token.Class]
 
 		if exists == false {
-			action = errorHandler(state, line, column, token)
+			action = errorHandler(state, &line, &column, token, filePtr)
 		}
 
 		if newState, err := strconv.Atoi(action); err != nil {
+
 			if action == "ACC" && token.Class == "EOF" {
 				fmt.Println("P' -> P")
 				return
@@ -64,59 +65,71 @@ func Reduce(action string) {
 	trash(exists)
 }
 
-func errorHandler(state, line, column int, token Token) (action string) {
+func errorHandler(state int, line *int, column *int, token Token, filePtr *os.File) (action string) {
 
 	if state == 0 {
-		errorPrinter("inicio", token, line, column)
-		return errorCorrector(state, line, column, token, "inicio")
+		errorPrinter("inicio", token, *line, *column)
+		return errorCorrector(state, line, column, token, "inicio", filePtr)
 	} else if state == 1 {
-		errorPrinter("EOF", token, line, column)
-		return errorCorrector(state, line, column, token, "EOF")
+		errorPrinter("EOF", token, *line, *column)
+		return errorCorrector(state, line, column, token, "EOF", filePtr)
 	} else if state == 2 {
-		errorPrinter("varinicio", token, line, column)
-		return errorCorrector(state, line, column, token, "varinicio")
+		errorPrinter("varinicio", token, *line, *column)
+		return errorCorrector(state, line, column, token, "varinicio", filePtr)
 	} else if state == 4 || state == 19 {
-		errorPrinter("varfim\" or \"inteiro\" or \"real\" or \"literal", token, line, column)
+		errorPrinter("varfim\" or \"inteiro\" or \"real\" or \"literal", token, *line, *column)
+		follow := [6]string{"varfim", "inteiro", "real", "literal", "", ""}
+		return errorPanic(follow, filePtr, line, column, state)
 	} else if state == 11 || state == 21 || state == 67 {
-		errorPrinter("ID", token, line, column)
-		return errorCorrector(state, line, column, token, "ID")
+		errorPrinter("ID", token, *line, *column)
+		return errorCorrector(state, line, column, token, "ID", filePtr)
 	} else if state == 12 {
-		errorPrinter("LIT\" or \"NUM\" or \"ID", token, line, column)
+		errorPrinter("LIT\" or \"NUM\" or \"ID", token, *line, *column)
+		follow := [6]string{"LIT", "NUM", "ID", "", "", ""}
+		return errorPanic(follow, filePtr, line, column, state)
 	} else if state == 13 {
-		errorPrinter("RCB", token, line, column)
-		return errorCorrector(state, line, column, token, "RCB")
+		errorPrinter("RCB", token, *line, *column)
+		return errorCorrector(state, line, column, token, "RCB", filePtr)
 	} else if state == 16 || state == 17 {
-		errorPrinter("AB_P", token, line, column)
-		return errorCorrector(state, line, column, token, "AB_P")
+		errorPrinter("AB_P", token, *line, *column)
+		return errorCorrector(state, line, column, token, "AB_P", filePtr)
 	} else if state == 20 || state == 29 || state == 30 || state == 49 || state == 53 {
-		errorPrinter("PT_V", token, line, column)
-		return errorCorrector(state, line, column, token, "PT_V")
+		errorPrinter("PT_V", token, *line, *column)
+		return errorCorrector(state, line, column, token, "PT_V", filePtr)
 	} else if state == 34 || state == 45 || state == 46 || state == 69 || state == 71 {
-		errorPrinter("ID\" or \"NUM", token, line, column)
+		errorPrinter("ID\" or \"NUM", token, *line, *column)
+		follow := [6]string{"ID", "NUM", "", "", "", ""}
+		return errorPanic(follow, filePtr, line, column, state)
 	} else if state == 54 {
-		errorPrinter("OPM", token, line, column)
-		return errorCorrector(state, line, column, token, "OPM")
+		errorPrinter("OPM", token, *line, *column)
+		return errorCorrector(state, line, column, token, "OPM", filePtr)
 	} else if state == 63 || state == 65 {
-		errorPrinter("FC_P", token, line, column)
-		return errorCorrector(state, line, column, token, "FC_P")
+		errorPrinter("FC_P", token, *line, *column)
+		return errorCorrector(state, line, column, token, "FC_P", filePtr)
 	} else if state == 64 {
-		errorPrinter("OPR", token, line, column)
-		return errorCorrector(state, line, column, token, "OPR")
+		errorPrinter("OPR", token, *line, *column)
+		return errorCorrector(state, line, column, token, "OPR", filePtr)
 	} else if state == 70 {
-		errorPrinter("entao", token, line, column)
-		return errorCorrector(state, line, column, token, "entao")
+		errorPrinter("entao", token, *line, *column)
+		return errorCorrector(state, line, column, token, "entao", filePtr)
 	} else if state == 3 || state == 6 || state == 7 || state == 8 || state == 9 {
-		errorPrinter("fim\" or \"leia\" or \"escreva\" or \"ID\" or \"se\" or \"repita", token, line, column)
+		errorPrinter("fim\" or \"leia\" or \"escreva\" or \"ID\" or \"se\" or \"repita", token, *line, *column)
+		follow := [6]string{"fim", "leia", "escreva", "ID", "se", "repita"}
+		return errorPanic(follow, filePtr, line, column, state)
 	} else if state == 14 || state == 36 || state == 37 || state == 38 {
-		errorPrinter("fimse\" or \"leia\" or \"escreva\" or \"ID\" or \"se", token, line, column)
+		errorPrinter("fimse\" or \"leia\" or \"escreva\" or \"ID\" or \"se", token, *line, *column)
+		follow := [6]string{"fimse", "leia", "escreva", "ID", "se", ""}
+		return errorPanic(follow, filePtr, line, column, state)
 	} else if state == 15 || state == 41 || state == 42 || state == 43 {
-		errorPrinter("fimrepita\" or \"leia\" or \"escreva\" or \"ID\" or \"se", token, line, column)
+		errorPrinter("fimrepita\" or \"leia\" or \"escreva\" or \"ID\" or \"se", token, *line, *column)
+		follow := [6]string{"fimrepita", "leia", "escreva", "ID", "se", ""}
+		return errorPanic(follow, filePtr, line, column, state)
 	}
 
 	panic("should never happen")
 }
 
-func errorCorrector(state, line, column int, oldToken Token, fixToken string) (newAction string) {
+func errorCorrector(state int, line, column *int, oldToken Token, fixToken string, filePtr *os.File) (newAction string) {
 
 	action, _ := SLR[state][fixToken]
 
@@ -130,15 +143,30 @@ func errorCorrector(state, line, column int, oldToken Token, fixToken string) (n
 	newAction, exist := SLR[newState][oldToken.Class]
 
 	if exist == false {
-		newAction = errorHandler(newState, line, column, oldToken)
+		newAction = errorHandler(newState, line, column, oldToken, filePtr)
 	}
 
 	return
 }
 
-func errorPanic() {
+func errorPanic(follow [6]string, filePtr *os.File, line, column *int, state int) (action string) {
 
+	for true {
+		token := SCANNER(filePtr, line, column)
+
+		if token.Class == "EOF" {
+			color.Red("Reached EOF")
+			os.Exit(0)
+		}
+		for i := 0; i < 6; i++ {
+			if follow[i] == token.Class {
+				return SLR[state][token.Class]
+			}
+		}
+	}
+	panic("should never happen")
 }
+
 func errorPrinter(expected string, token Token, line, column int) {
 
 	color.Red("SYNTACTIC ERROR - Expected \"%s\", found \"%s\"\nLine:%d\tColumn:%d", expected, token.Class, line, column-1)
